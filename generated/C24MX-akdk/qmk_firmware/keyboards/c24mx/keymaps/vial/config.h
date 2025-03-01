2d4821d4a3dc1d4f3e48881522d6b8458ff66fc2
fu2hito <14793610+fu2hito@users.noreply.github.com>
2025-03-01 06:41:37 +0000

    Add initial configuration and files for C24MX, C24V2, C35SPMX, and C35SPV2 keyboards

---
+++ generated/C24MX-akdk/qmk_firmware/keyboards/c24mx/keymaps/vial/config.h
@@ -0,0 +1,5 @@
+#pragma once
+#define VIAL_KEYBOARD_UID {0x67, 0x4a, 0x0d, 0x24, 0xdf, 0x84, 0x78, 0x1f}
+#define VIAL_TAP_DANCE_ENTRIES 8
+#define VIAL_COMBO_ENTRIES 8
+#define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)
---
+++ generated/C24V2-akdk/qmk_firmware/keyboards/c24v2/keymaps/vial/config.h
@@ -0,0 +1,5 @@
+#pragma once
+#define VIAL_KEYBOARD_UID {0x08, 0xf1, 0x4f, 0xc2, 0xfc, 0xad, 0xdd, 0xea}
+#define VIAL_TAP_DANCE_ENTRIES 8
+#define VIAL_COMBO_ENTRIES 8
+#define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)
